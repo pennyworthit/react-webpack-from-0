5cadde29a644d66338b93d4cd3c148fc4adc7874
Monsoir <monwingyeung@gmail.com>
2018-09-07 00:47:49 +0800

    feat: 使用 Webpack 打包 JS 与 JSX 代码

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -4,11 +4,27 @@ const rootPath = path.join(__dirname, '..');
 const publicPath = path.join(rootPath, 'public');
 
 module.exports = {
-  entry: path.join(rootPath, 'src/app.js'),
+  entry: path.join(rootPath, 'src/index.js'),
   output: {
     filename: '[name].[hash].js',
     path: path.join(rootPath, 'build'),
     publicPath,
   },
   mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/env', '@babel/react'],
+          },
+        },
+        exclude: [
+          path.join(rootPath, 'node_modules'),
+        ],
+      },
+    ],
+  },
 };
