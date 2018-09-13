01a835df1e74eccfeb4dbdf7fc183f3911a43297
Monsoir <monwingyeung@gmail.com>
2018-09-13 14:08:51 +0800

    feat: 使用 Webpack 默认的代码分割设置，添加 Content 自定义组件

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import Content from './Content.jsx';
 
 export default class App extends React.Component {
   render() {
     return (
-      <div>An App</div>
+      <div>
+        <div>An App</div>
+        <Content />
+      </div>
     );
   }
 }
